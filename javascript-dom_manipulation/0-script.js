20199ccf7b27cfbdb79b77296ffc82068713799d
Maly <jhamilamedina98xd@gmail.com>
2024-03-13 19:08:02 -0500

    Cambiar a rojo el header

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// Usar document.querySelector para seleccionar la etiqueta HTML
+const header = document.querySelector("header");
+
+// Actualizar el color del texto del header a rojo (#FF0000)
+header.style.color = "#FF0000";
