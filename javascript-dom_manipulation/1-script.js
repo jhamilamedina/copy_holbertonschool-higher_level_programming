6df8de5da383d85cad2a47a8f1aadecf77e740f8
Maly <jhamilamedina98xd@gmail.com>
2024-03-13 19:31:31 -0500

    Fix: Cambio de color al hacer click en el header

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleccionamos el elemento con el id read_header
+    const id = document.getElementById("red_header");
+
+    // Agregamos un evento de click en el elemento red_header
+    red_header.addEventListener("click", function() {
+
+        // Usar document.querySelector para seleccionar la etiqueta HTML
+        const header = document.querySelector("header");
+        // Actualizar el color del texto del header a rojo (#FF0000)
+        header.style.color = "#FF0000";
+        
+    });
+});
