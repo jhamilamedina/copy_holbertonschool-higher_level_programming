e1b795ceedd61d0a0bccd4514e040d60ff75b78f
Maly <jhamilamedina98xd@gmail.com>
2024-03-31 13:14:42 -0500

    feat:Agregar un evento de click a red header para cambiar de color a rojo

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,10 @@
+// Actualizar el header a rojo cuando haga click en DIV#red_header
+$(document).ready(function() {
+
+  // Agrega un controlador de eventos a red_header
+  $("#red_header").click(function() {
+
+    // Selecciona el header a cambia de color a rojo
+    $("header").css("color", "#FF0000");
+  });
+});
