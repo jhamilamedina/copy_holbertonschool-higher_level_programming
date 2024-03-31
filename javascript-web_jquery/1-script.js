c597a8f40d9751fbbdc99b1baeea7956f214dd37
Maly <jhamilamedina98xd@gmail.com>
2024-03-31 01:59:00 -0500

    feat: Cambiar el header a rojo utilizando JQuery

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+// Actualizar el header a rojo usando JQuery
+$(document).ready(function() {
+  const headerElement = $('header');
+
+  // Actualizar el texto a rojo
+  headerElement.css('color', '#FF0000');
+})
