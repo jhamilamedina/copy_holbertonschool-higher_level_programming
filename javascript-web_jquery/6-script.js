e7e1730e77aa689b2c0cdcfb8a6dee0009d16eb2
Maly <jhamilamedina98xd@gmail.com>
2024-04-02 23:55:38 -0500

    feat: Cambio del header al texto New Headergit add 6-script.js !

--- javascript-web_jquery/6-script.js
+++ javascript-web_jquery/6-script.js
@@ -1,10 +1,8 @@
 // Esperar a que el DOM este listo
-$(document).ready(function() {
-
+$(document).ready(() => {
   // Agregamos un click de eventos al update_header
-  $("#update_header").click(function() {
-
+  $('#update_header').click(() => {
     // Actualizamos el texto del header
-    $("header").text("New Header!!!");
+    $('header').text('New Header!!!');
   });
 });
