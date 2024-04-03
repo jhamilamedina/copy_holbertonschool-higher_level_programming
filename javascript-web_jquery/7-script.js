726caa3ae819cddb7d96184c020d68a060dbabf1
Maly <jhamilamedina98xd@gmail.com>
2024-04-02 23:57:07 -0500

    feat: Obtener el nombre del titulo de la pelicula

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,19 +1,18 @@
 // Obtener el nombre del personaje
-$(document).ready(function(){
-
+$(document).ready(() => {
   // Utilizamos el metodo $.ajax y hacemos una solicitud GET
-  $.ayax ({
+  $.ayax({
     URL: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
     method: 'GET',
-    success: function(response) { //se ejecutará si la solicitud es exitosa
+    success(response) { // se ejecutará si la solicitud es exitosa
       // Accedemos al nombre del personaje en el objeto JSON devuelto por el API
-      let characterName = response.name;
+      const characterName = response.name;
       // Utilizamos el método .html() de jQuery para establecer el contenido del div con id "character"
       $('#character').text(characterName);
-  },
-  error: function() { // La función que se ejecutará si la solicitud falla
+    },
+    error() { // La función que se ejecutará si la solicitud falla
       // En caso de error, mostramos un mensaje de error en el div con id "character"
       $('#character').text('Error al cargar el nombre del personaje:', error);
-  }
-});
+    },
+  });
 });
