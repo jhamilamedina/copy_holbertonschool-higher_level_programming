b5a5b338e4d3f445a8f666253ba59f158d131df0
Maly <jhamilamedina98xd@gmail.com>
2024-04-22 00:12:30 -0500

    feat: Obtener el nombre del personaje

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,18 +1,11 @@
-// Obtener el nombre del personaje
-$(document).ready(() => {
-  // Utilizamos el metodo $.ajax y hacemos una solicitud GET
-  $.ajax({
-    URL: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
-    method: 'GET',
-    success(response) { // se ejecutará si la solicitud es exitosa
-      // Accedemos al nombre del personaje en el objeto JSON devuelto por el API
-      const characterName = response.name;
-      // Utilizamos el método .html() de jQuery para establecer el contenido del div con id "character"
-      $('#character').text(characterName);
-    },
-    error() { // La función que se ejecutará si la solicitud falla
-      // En caso de error, mostramos un mensaje de error en el div con id "character"
-      $('#character').text('Error al cargar el nombre del personaje:', error);
-    },
+// Espera a que el documento HTML esté completamente cargado
+$(document).ready(function() {
+  // Realiza una solicitud GET a la API de SWAPI
+  $.get("https://swapi-api.hbtn.io/api/people/5/?format=json", function(data) {
+      // Extrae el nombre del personaje del objeto JSON
+      var characterName = data.name;
+      
+      // Inserta el nombre del personaje en el elemento <div id="character">
+      $("#character").text(characterName);
   });
 });
